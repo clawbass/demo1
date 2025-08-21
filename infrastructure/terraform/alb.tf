b5cf3752e82233d2ebf36782fdb92c56818a336e
clawbass <clawbass@gmail.com>
2025-08-21 15:22:59 +0200

    Create alb.tf

---
+++ infrastructure/terraform/alb.tf
@@ -0,0 +1,36 @@
+resource "aws_lb" "this" {
+  name               = "${var.project}-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.public.ids
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+}
+
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.project}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
+  health_check { path = "/" matcher = "200-399" }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port = 80
+  protocol = "HTTP"
+  default_action { type = "redirect" redirect { port = "443" protocol = "HTTPS" status_code = "HTTP_301" } }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port = 443
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-TLS13-1-2-Ext1-2021-06"
+  certificate_arn = aws_acm_certificate.cert.arn
+  default_action { type = "forward" target_group_arn = aws_lb_target_group.tg.arn }
+}
