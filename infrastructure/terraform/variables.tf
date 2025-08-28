f7c43411b93ac68b9a02d893d55278bc61e5647d
clawbass <clawbass@gmail.com>
2025-08-28 13:11:50 +0200

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "project" { default = "dash-staging" }
 variable "region"  { default = "eu-central-1" }
-variable "domain_name" {demo.demo.com}         # e.g., staging.example.com
-variable "alert_email"  {111@demo.com}        # email for SNS
+variable "domain_name" {demo.demo.com}         
+variable "alert_email"  {111@demo.com}       
