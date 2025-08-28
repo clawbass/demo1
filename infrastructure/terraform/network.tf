b3f000bb01c630d6f01a9973170c020f5b5d0e2d
clawbass <clawbass@gmail.com>
2025-08-28 13:11:22 +0200

    Update network.tf

--- infrastructure/terraform/network.tf
+++ infrastructure/terraform/network.tf
@@ -2,7 +2,7 @@ data "aws_vpc" "default" { default = true }
 data "aws_subnets" "public" { filter { name="vpc-id" values=[data.aws_vpc.default.id] } }
 
 resource "aws_security_group" "alb" {
-  name   = "${var.project}-alb-sg"
+  name   = "demo1-alb-sg"
   vpc_id = data.aws_vpc.default.id
   ingress { from_port=80  to_port=80  protocol="tcp" cidr_blocks=["0.0.0.0/0"] }
   ingress { from_port=443 to_port=443 protocol="tcp" cidr_blocks=["0.0.0.0/0"] }
@@ -10,7 +10,7 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group" "service" {
-  name   = "${var.project}-svc-sg"
+  name   = "demo1-svc-sg"
   vpc_id = data.aws_vpc.default.id
   ingress { from_port=80 to_port=80 protocol="tcp" security_groups=[aws_security_group.alb.id] }
   egress  { from_port=0  to_port=0  protocol="-1"  cidr_blocks=["0.0.0.0/0"] }
